ea5280a3b05c4d9e43f6438f963c1482991d34dc
Akash Sarkar <akashsarker1433@gmail.com>
2021-07-02 10:21:22 +0600

    Initial Commit

---
+++ Tic_Tac_Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic_Tac_Toe
+//
+//  Created by Akash Sarkar on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
