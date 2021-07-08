f040f58a33ce57f2c07df744bfb79c552e4fef73
Akash Sarkar <akashsarker1433@gmail.com>
2021-07-08 18:25:19 +0600

    win condition and block condition

--- Tic_Tac_Toe/ContentView.swift
+++ Tic_Tac_Toe/ContentView.swift
@@ -80,6 +80,44 @@ struct ContentView: View {
     }
     
     func determainComputerMovePosition(in moves: [Move?]) -> Int {
+        // If AI can win then win
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                                          [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let computerMoves = moves.compactMap { $0 }.filter({ $0.player == .computer })
+        let computerPosition = Set(computerMoves.map({ $0.boardIndex }))
+        
+        for patern in winPatterns {
+            let winPosition = patern.subtracting(computerPosition)
+            
+            if winPosition.count == 1{
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPosition.first!)
+                
+                if isAvailable {
+                    return winPosition.first!
+                }
+            }
+        }
+        
+        // If AI can block then block
+        let humanMoves = moves.compactMap { $0 }.filter({ $0.player == .human })
+        let humanPosition = Set(humanMoves.map({ $0.boardIndex }))
+        
+        for patern in winPatterns {
+            let winPosition = patern.subtracting(humanPosition)
+            
+            if winPosition.count == 1{
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPosition.first!)
+                
+                if isAvailable {
+                    return winPosition.first!
+                }
+            }
+        }
+        
+        // If AI can't block then take middle
+        
+        // if AI cant take middle then take random available square
         var movePosition = Int.random(in: 0..<9)
         
         while(isSquareOccupied(in: moves, forIndex: movePosition)) {
